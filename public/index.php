8ab356e75b2ac365067598501594717a53bf0e3a
Renar Suurpere <renar.suurpere@khk.ee>
2021-09-09 11:34:40 +0300

    Created search with POST method

--- public/index.php
+++ public/index.php
@@ -25,4 +25,6 @@ $app->get('/', 'App\Controller\AlbumsController:default');
 
 $app->get('/search', 'App\Controller\AlbumsController:search');
 
+$app->any('/form', 'App\Controller\AlbumsController:form');
+
 $app->run();
--- src/Controller/AlbumsController.php
+++ src/Controller/AlbumsController.php
@@ -29,4 +29,21 @@ class AlbumsController extends Controller
     		'query' => $query,
     		'albums' => $albums]);
 	}
+
+	public function form(Request $request, Response $response)
+	{
+		$albums = json_decode(file_get_contents(__DIR__ . '/../../data/albums.json'),true);
+
+		$query = $request->getParam('q');
+
+		if($request->isPost()){
+			$albums = array_values(array_filter($albums, function($album) use ($query){
+				return strpos($album['title'], $query) !== false or strpos($album['artist'], $query) !== false;
+			}));
+		}
+
+    	return $this->render($response, 'form.html', [
+    		'query' => $query,
+    		'albums' => $albums]);
+	}
 }
